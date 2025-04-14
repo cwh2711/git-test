116dd6d4053c0119fa69ec1395ac3d61002ba5ad
Tiffany Chen (陳文忻) <tiffany_wh_chen@GSS.COM.TW>
2025-04-14 11:26:44 +0800

    add b=c and string

--- test1.c
+++ test1.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
    char a,b,c;
    scanf("%s",&a);
+   b=c;
+   printf("%s")
    printf("hello world\n");
    printf("%s\n",a);
    printf("ok");
