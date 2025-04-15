97e7db03dfadfee1be48a4c1076955605ee0e819
Tiffany Chen (陳文忻) <tiffany_wh_chen@GSS.COM.TW>
2025-04-15 10:31:31 +0800

    增加副函式

--- test1.c
+++ test1.c
@@ -1,13 +1,30 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+int re(int b,int c)
+{
+
+   if (b<c)
+   {
+      return 1 + re(b + 1, c);
+   }
+   else
+   {
+      return 0;
+   }
+
+}
 int main()
 {
-   char a,b,c;
-   scanf("%10s",&a);
-   b=c;
-   printf("%s",a);
-   printf("hello world\n");
+   char a[10];
+   int b,c,d;
+   scanf("%s",&a);
+   scanf("%2d %2d",&b,&c);
+   d=re(b,c);
+   printf("a=");
    printf("%s\n",a);
+   printf("d=%d",d);
+   printf("\n");
    printf("ok");
    return 0;
 }
